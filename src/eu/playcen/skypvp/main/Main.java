1e4be47add4fbffa972aa12080c4e5f6d667ec2f
Adam <aeromcde@gmail.com>
2020-08-15 01:06:11 +0200

    Update Gamemode

--- src/eu/playcen/skypvp/main/Main.java
+++ src/eu/playcen/skypvp/main/Main.java
@@ -1,5 +1,6 @@
 package eu.playcen.skypvp.main;
 
+import eu.playcen.skypvp.commands.CMD_Gamemode;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -8,4 +9,15 @@ public class Main extends JavaPlugin {
     noperm = "§cDiesen Berfehl darfst du nicht benutzen!",
     cmdnotfound = "§cDieser Befehl wurde nicht gefunden!",
     cmdp = "§Diesen Befehl dürfen nur Spieler benutzen!";
+
+    @Override
+    public void onEnable() {
+
+        //Commands
+        getCommand("gamemode").setExecutor(new CMD_Gamemode());
+
+
+        //Listener & Events
+
+    }
 }
