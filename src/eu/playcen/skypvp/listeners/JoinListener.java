3526f466b04ebfa3745dc3491a657e0a67a49c6b
Adam <aeromcde@gmail.com>
2020-08-15 01:09:01 +0200

    Update

--- src/eu/playcen/skypvp/listeners/JoinListener.java
+++ src/eu/playcen/skypvp/listeners/JoinListener.java
@@ -1,5 +1,7 @@
 package eu.playcen.skypvp.listeners;
 
+import eu.playcen.skypvp.main.Main;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -7,7 +9,12 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class JoinListener implements Listener {
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent e){
+    public void onJoin(PlayerJoinEvent event){
+
+        Player p = event.getPlayer(); //Den Spieler vom Event Deklariert
+
+        //Join Message
+        event.setJoinMessage(Main.pre + "Willkommen!");
 
     }
 }
