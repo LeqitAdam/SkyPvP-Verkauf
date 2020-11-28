ddb50eeacb9e3b56ff88f6b93127544efeccd361
Mäx <Primax4K>
2020-11-28 21:05:11 +0100

    Join

---
+++ src/eu/playcen/skypvp/commands/CMD_Fly.java
@@ -0,0 +1,39 @@
+package eu.playcen.skypvp.commands;
+
+import eu.playcen.skypvp.main.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CMD_Fly implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(sender instanceof Player){
+            Player p = (Player) sender;
+            if(args.length == 0){
+                if(p.hasPermission("skypvp.fly")){
+                    p.setAllowFlight(!p.getAllowFlight());
+                    return true;
+                }
+            }
+            if(args.length == 1) {
+                if (p.hasPermission("skypvp.fly.other")){
+                    if (Bukkit.getServer().getPlayer(args[0]) != null) {
+                        Player target = Bukkit.getServer().getPlayer(args[0]);
+                        target.setAllowFlight(!target.getAllowFlight());
+                        return true;
+                    } else {
+                        p.sendMessage(Main.pre + "Dieser Spieler ist nicht online!");
+                        return true;
+                    }
+                }
+            }
+            else {
+                p.sendMessage(Main.pre + "/fly <user>");
+            }
+        }
+        return false;
+    }
+}
--- src/eu/playcen/skypvp/main/Main.java
+++ src/eu/playcen/skypvp/main/Main.java
@@ -26,6 +26,7 @@ public class Main extends JavaPlugin {
         getCommand("tp").setExecutor(new CMD_Tp(this));
         getCommand("tphere").setExecutor(new CMD_Tphere(this));
         getCommand("tppos").setExecutor(new CMD_Tppos());
+        getCommand("fly").setExecutor(new CMD_Fly());
 
         /*
          /fly
