439400654d2a695eb4b7f7671c5892bd3cfb9ec8
Adam <aeromcde@gmail.com>
2020-11-29 12:31:03 +0100

    Bugfixes und Prefix update

--- src/eu/playcen/skypvp/main/Main.java
+++ src/eu/playcen/skypvp/main/Main.java
@@ -1,36 +1,19 @@
 package eu.playcen.skypvp.main;
 
-import com.avaje.ebean.SqlRow;
 import eu.playcen.skypvp.commands.*;
+import eu.playcen.skypvp.listeners.InventoryClickListener;
+import eu.playcen.skypvp.listeners.JoinListener;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Main extends JavaPlugin {
-
-    private void createConfig() {
-
-        File file = new File("plugins/SkyPvP", "config.yml");
-
-        if(file.exists()) return;
-
-        YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
-
-        cfg.set("Prefix", "&9SkyPvP &7»");
-        cfg.set("menutitle", "&9SkyPvP §8- §eKits");
-
-        try {
-            cfg.save(file);
-            System.out.println("[SkyPvP] Config wurde erstellt!");
-        } catch (IOException e) {
-            System.out.println("[SkyPvP] Config wurde nicht erstellt");
-            e.printStackTrace();
-        }
-
-    }
 
     public static String
     noperm = " §cDiesen Berfehl darfst du nicht benutzen!",
@@ -43,6 +26,9 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
+        createConfig();
+        plugin = this;
 
         //Commands
         getCommand("gamemode").setExecutor(new CMD_Gamemode());
@@ -73,12 +59,35 @@ public class Main extends JavaPlugin {
 
 
         //Listener & Events
+        PluginManager pm = Bukkit.getPluginManager();
+        pm.registerEvents(new JoinListener(), this);
+        pm.registerEvents(new InventoryClickListener(), this);
 
     }
 
+    private void createConfig() {
 
+        File file = new File("plugins/SkyPvP", "config.yml");
+
+        if(file.exists()) return;
+
+        YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
+
+        cfg.set("Prefix", "&9SkyPvP &7»");
+        cfg.set("menutitle", "&9SkyPvP §8- §eKits");
+
+        try {
+            cfg.save(file);
+            System.out.println("[SkyPvP] Config wurde erstellt!");
+        } catch (IOException e) {
+            System.out.println("[SkyPvP] Config wurde nicht erstellt");
+            e.printStackTrace();
+        }
+
+    }
 
     public static Main getPlugin() {
         return plugin;
     }
+
 }
