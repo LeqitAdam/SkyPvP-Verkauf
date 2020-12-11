004a1d97a21517a38d39eb0234a1651800c7068c
Mäx <Primax4K>
2020-12-11 19:45:38 +0100

    Kit

---
+++ src/eu/playcen/skypvp/kit/Kits.java
@@ -0,0 +1,4 @@
+package eu.playcen.skypvp.kit;
+
+public class Kits {
+}
--- src/eu/playcen/skypvp/listeners/Environment.java
+++ src/eu/playcen/skypvp/listeners/Environment.java
@@ -1,10 +1,12 @@
 package eu.playcen.skypvp.listeners;
 
 import eu.playcen.skypvp.commands.CMD_Build;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerAchievementAwardedEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
@@ -26,11 +28,16 @@ public class Environment implements Listener {
     public void onInteract(PlayerInteractEvent e){
         if(!CMD_Build.builders.contains(e.getPlayer()))
             e.setCancelled(true);
-
     }
 
     @EventHandler
     public void onAchieve(PlayerAchievementAwardedEvent e){
         e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void entityDeathEvent(EntityDeathEvent e){
+        if(e.getEntity().getType() != EntityType.PLAYER)
+            e.getDrops().clear();
     }
 }
