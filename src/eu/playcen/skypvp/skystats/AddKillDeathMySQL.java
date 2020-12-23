26a1ac331f079f4fe2a5cd5b3d5f5f37c307b955
Mäx <Primax4K>
2020-12-23 21:12:03 +0100

    Scoreboard

--- src/eu/playcen/skypvp/skystats/AddKillDeathMySQL.java
+++ src/eu/playcen/skypvp/skystats/AddKillDeathMySQL.java
@@ -1,9 +1,11 @@
 package eu.playcen.skypvp.skystats;
 
+import eu.playcen.coins.api.CoinsAPI;
 import eu.playcen.skypvp.main.Main;
 import eu.playcen.skypvp.methods.ScoreboardMethod;
 import eu.playcen.skypvp.mysql.MySQL;
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,10 +20,12 @@ public class AddKillDeathMySQL implements Listener {
         Player killer = event.getEntity().getPlayer().getKiller();
         if(MySQL.isConnected()){
             SkyStatsMethod.updateDeaths(p.getUniqueId(), 1, false, p.getName());
+            CoinsAPI.addCoins(p.getUniqueId(), -5);
             for(Player all : Bukkit.getOnlinePlayers()) {
                 ScoreboardMethod.setScoreBoard(all);
             }
         }
+        p.playSound(p.getLocation(), Sound.CAT_HISS, 1, 1);
         if(killer != null) {
             event.setDeathMessage(Main.prefix + " §7Der Spieler §a" + p.getDisplayName() + " §7wurde von §a" + killer.getDisplayName() + " §cgetötet!");
         }else
@@ -30,13 +34,14 @@ public class AddKillDeathMySQL implements Listener {
 
     @EventHandler
     public void onKill(EntityDeathEvent event){
-        if(event.getEntity().getKiller() != null){
+        if(event.getEntity().getKiller() != null && event.getEntity() instanceof Player){
             Player p = event.getEntity().getKiller();
             SkyStatsMethod.updateKills(p.getUniqueId(), 1, false, p.getName());
-
+            CoinsAPI.addCoins(p.getUniqueId(), 10);
             for(Player all : Bukkit.getOnlinePlayers()) {
                 ScoreboardMethod.setScoreBoard(all);
             }
+            p.playSound(p.getLocation(), Sound.LEVEL_UP, 1, 1);
         }
     }
 }
