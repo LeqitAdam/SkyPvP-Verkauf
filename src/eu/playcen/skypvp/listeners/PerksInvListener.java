876750c08d659d53530bca8a22fbd1bf44ef161b
Adam <aeromcde@gmail.com>
2021-01-06 19:16:04 +0100

    Update

--- src/eu/playcen/skypvp/listeners/PerksInvListener.java
+++ src/eu/playcen/skypvp/listeners/PerksInvListener.java
@@ -26,19 +26,19 @@ public class PerksInvListener implements Listener {
                     File file = new File("plugins/SkyPvP", "config.yml");
                     YamlConfiguration cfg2 = YamlConfiguration.loadConfiguration(file);
                     int height = cfg2.getInt("PvPHoehe");
-
-                    if(!p.hasPermission("skypvp.*") || !p.hasPermission("skypvp.perks.bypass")) {
-                        if(p.getLocation().getBlockY() >= (height)) {
-                            PerksMethod.flyperk(p);
-                        }else
-                            p.sendMessage(Main.prefix + " §cDu darfst dieses §ePerk §chier nicht benutzen!");
+                    if(p.hasPermission("skypvp.perks.fly")) {
+                        if(!p.hasPermission("skypvp.*") || !p.hasPermission("skypvp.perks.bypass") || !p.hasPermission("skypvp.fly.bypass")) {
+                            if(p.getLocation().getBlockY() >= (height)) {
+                                PerksMethod.flyperk(p);
+                            }else
+                                p.sendMessage(Main.prefix + " §cDu darfst dieses §ePerk §chier nicht benutzen!");
                             if(p.isFlying()) {
                                 p.setAllowFlight(false);
                             }
-                    }else if(p.hasPermission("skypvp.perks.fly") || p.hasPermission("skypvp.fly.bypass")) {
-                        PerksMethod.flyperk(p);
+                        }else
+                            PerksMethod.flyperk(p);
                     }else
-                        p.sendMessage(Main.prefix + " §cDieses §ePerk §cdarfst du nicht benutzen!");
+                        p.sendMessage(Main.prefix + " §7Dieses §ePerk §7hast du §cnicht §afreigeschaltet!");
                 }else if(event.getCurrentItem().getItemMeta().getLore().contains("§9JumpBoost §aaktiv!") || event.getCurrentItem().getItemMeta().getLore().contains("§9JumpBoost §cinaktiv!")) {
                     PerksMethod.jumpboostPerk(p);
                 }else if(event.getCurrentItem().getItemMeta().getLore().contains("§9Speed §aaktiv!") || event.getCurrentItem().getItemMeta().getLore().contains("§9Speed §cinaktiv!")) {
